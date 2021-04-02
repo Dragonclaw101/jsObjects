0ecfc73fb44e128e301ee7a1a8206caeb293bb90
Jo-Jo Hupp <huppjoesaphean@gmail.com>
2021-04-02 12:48:45 -0700

    Added lab 53

---
+++ scripts/object.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let myObj = {
+	
+	nameFirst: 'Daniel',
+	nameLast: 'Michael',
+	age: 17,
+	school: 'New York Public High School',
+	state: 'New York',
+	favFood: 'Pizza',
+
+};
+
+console.log('My person\'s first name is ' + myObj.nameFirst);
+console.log('My person\'s last name is ' + myObj.nameLast);
+console.log('My person\'s age is ' + myObj.age + ' years old');
+console.log('My person attends ' + myObj.school);
+console.log('My person lives in ' + myObj.state);
+console.log('My person\'s favorite food is ' + myObj.favFood);
