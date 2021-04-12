5f7bf4d09b6c0bad9ac01ffa67c85aaf245dcb41
Jo-Jo Hupp <huppjoesaphean@gmail.com>
2021-04-11 17:36:12 -0700

    Added lab 54.

--- scripts/object.js
+++ scripts/object.js
@@ -8,7 +8,8 @@ let myObj = {
 	school: 'New York Public High School',
 	state: 'New York',
 	favFood: 'Pizza',
-
+	favSubs: ['Chemistry', 'English', 'History'],
+	family: [{name:'Sally sister'}, {name:'Billy brother'}],
 };
 
 console.log('My person\'s first name is ' + myObj.nameFirst);
@@ -17,3 +18,5 @@ console.log('My person\'s age is ' + myObj.age + ' years old');
 console.log('My person attends ' + myObj.school);
 console.log('My person lives in ' + myObj.state);
 console.log('My person\'s favorite food is ' + myObj.favFood);
+
+console.log('My person family ' + myObj.family[1].name);
